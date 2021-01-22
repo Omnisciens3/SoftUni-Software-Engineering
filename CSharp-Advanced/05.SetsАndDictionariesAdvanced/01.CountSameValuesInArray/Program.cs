194ee75c95a1d9f9f7c425bdc10c13125518a88e
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-22 13:00:49 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced

---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> occurances = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!occurances.ContainsKey(numbers[i]))
+                {
+                    occurances.Add(numbers[i], 0);
+                }
+
+                occurances[numbers[i]]++;
+            }
+
+            foreach (var occurance in occurances)
+            {
+                Console.WriteLine($"{occurance.Key} - {occurance.Value} times");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/03.ProductShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Revision")
+            {
+                string[] splitted = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string shop = splitted[0];
+                string product = splitted[1];
+                double price = double.Parse(splitted[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+
+                shops[shop].Add(product, price);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var shop in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}
