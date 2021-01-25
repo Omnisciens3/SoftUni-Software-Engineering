410d92a8f1e3c3dd0a0da9a5dc0ea5ec51b24e26
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-25 11:17:23 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Custom HashSet)

---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/08.CustomHashSetImplementation/CustomHashSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CustomHashSetImplementation
+{
+    public class CustomHashSet
+    {
+        ulong size = 8;
+        private List<string>[] internalArray;
+        public CustomHashSet(int capacity = 8)
+        {
+            size = (ulong)capacity * 8;
+            internalArray = new List<string>[size];
+        }
+
+        public void Add(string element)
+        {
+            int index = HashFunction(element);
+
+            if (internalArray[index] == null)
+            {
+                internalArray[index] = new List<string>();
+                internalArray[index].Add(element);
+            }
+            else
+            {
+                internalArray[index].Add(element);
+            }
+        }
+
+        public bool Contains(string element)
+        {
+            int index = HashFunction(element);
+
+            if (internalArray[index] != null)
+            {
+                for (int i = 0; i < internalArray[index].Count; i++)
+                {
+                    if (internalArray[index][i] == element)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        private int HashFunction(string element)
+        {
+            //return Math.Abs(element.GetHashCode()) % size; - вградена хеш-функция от C# - много е добра също.
+
+            UInt64 hashedValue = 3074457345618258791ul;
+            for (int i = 0; i < element.Length; i++)
+            {
+                hashedValue += element[i];
+                hashedValue *= 3074457345618258799ul;
+            }
+
+            return (int)(hashedValue % size);
+        }
+    }
+}
