ae788fe2a592837f42776e0b9bf65af5f926ba6b
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-25 09:24:36 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Sets and Dictionaries)

---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/06.ParkingLot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] input = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = input[0];
+                string carNumber = input[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else
+                {
+                    cars.Remove(carNumber);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
