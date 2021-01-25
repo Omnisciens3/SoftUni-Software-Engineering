c5455062c6a2eb555b41c8b65495c29901a0d5fb
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-25 10:15:36 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Sets and Dictionaries)

---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/07.SoftUniParty/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "PARTY")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        if (char.IsDigit(command[i]))
+                        {
+                            vipGuests.Add(command); 
+                        }
+                        else
+                        {
+                            regularGuests.Add(command);
+                        }
+                        break;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        if (char.IsDigit(command[i]))
+                        {
+                            vipGuests.Remove(command);
+                        }
+                        else
+                        {
+                            regularGuests.Remove(command);
+                        }
+                        break;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
