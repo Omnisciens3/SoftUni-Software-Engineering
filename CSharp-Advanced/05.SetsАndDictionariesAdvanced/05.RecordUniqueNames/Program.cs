4f63b5b71d927d07c4269518d3e0387cd6f1dfe8
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-25 09:03:43 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Sets and Dictionaries)

---
+++ CSharp-Advanced/05.SetsАndDictionariesAdvanced/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                names.Add(input);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
