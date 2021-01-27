e1118b64a9883015f3936e79fecc7e67aa73849b
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-27 19:57:40 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Streams, Files and Directories)

---
+++ CSharp-Advanced/07.Streams-FilesAndDirectories/04.MergeFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = "../../../";
+            string firstFileName = "fileOne.txt";
+            string secondFileName = "fileTwo.txt";
+
+            string firstFilePath = Path.Combine(folderPath, firstFileName);
+            string secondFilePath = Path.Combine(folderPath, secondFileName);
+
+            var mergedList = new List<string>();
+
+            var firstFileLines = File.ReadAllLines(firstFilePath);
+            var secondFileLines = File.ReadAllLines(secondFilePath);
+
+            foreach (var line in firstFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            foreach (var line in secondFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            string outputFilePath = "output.txt";
+
+            File.WriteAllLines(outputFilePath, mergedList.OrderBy(x => x));
+        }
+    }
+}
