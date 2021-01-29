36666459d6b53d7285d086d7bc8d5d3ea96c2155
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-29 13:30:59 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Streams, Files and Directories)

---
+++ CSharp-Advanced/08.Streams-FilesAndDirectories-Exercise/05.DirectoryTraversal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> fileInfo = new Dictionary<string, Dictionary<string, double>>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../../");
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (!fileInfo.ContainsKey(file.Extension))
+                {
+                    fileInfo.Add(file.Extension, new Dictionary<string, double>());
+                }
+
+                fileInfo[file.Extension].Add(file.Name, file.Length / 1024.00);
+            }
+
+            using (StreamWriter writer = new StreamWriter
+                (@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\report.txt"))
+            {
+                foreach (var extension in fileInfo.OrderByDescending(f => f.Value.Count).ThenBy(e => e.Key))
+                {
+                    writer.WriteLine(extension.Key);
+
+                    foreach (var item in extension.Value.OrderBy(i => i.Value))
+                    {
+                        writer.WriteLine($"--{item.Key} - {item.Value}kb");
+                    }
+                }
+            }
+        }
+    }
+}
