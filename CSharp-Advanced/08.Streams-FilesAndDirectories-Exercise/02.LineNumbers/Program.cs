ad89d33f8c93e488f9d3a53555841fa2023b4d33
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-29 13:30:04 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Streams, Files and Directories)

---
+++ CSharp-Advanced/08.Streams-FilesAndDirectories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+
+            string[] result = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                result[i] = $"Line {i + 1}: {line} ({CountOfLetters(line)})({CountOfPunctuationMarks(line)})";
+            }
+
+            File.WriteAllLines("../../../output.txt", result);
+        }
+
+        static int CountOfLetters(string line)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currentSymbol = line[i];
+
+                if (char.IsLetter(currentSymbol))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static int CountOfPunctuationMarks(string line)
+        {
+            char[] punctuationMarks = { '-', ',', '.', '!', '?', '\'', ':', ';' };
+
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currentSymbol = line[i];
+
+                if (punctuationMarks.Contains(currentSymbol))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
