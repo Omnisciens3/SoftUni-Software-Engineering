0ba089bdfa9c1ebdff3d0557231c66e3e5d23b7d
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-29 13:29:10 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Streams, Files and Directories)

---
+++ CSharp-Advanced/08.Streams-FilesAndDirectories-Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int lineCounter = 0;
+
+                    while (line != null)
+                    {
+                        if (lineCounter % 2 == 0)
+                        {
+                            Regex pattern = new Regex("[-,.!?]");    //{"-", ",", ".", "!", "?"}  --> @
+                            line = pattern.Replace(line, "@");
+
+                            var array = line.Split().ToArray().Reverse();
+
+                            writer.WriteLine(string.Join(" ", array));
+                        }
+
+                        lineCounter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
