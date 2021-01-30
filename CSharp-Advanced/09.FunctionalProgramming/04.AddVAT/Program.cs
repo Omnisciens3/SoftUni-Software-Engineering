28d1cff3933fe99a5853d4939b6eafddc11fd165
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-30 21:02:19 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/09.FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine()
+                                                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(double.Parse)
+                                                .Select(price => price += price * 0.2) //Select.
+                                                .ToArray();
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f}");
+            }
+        }
+    }
+}
