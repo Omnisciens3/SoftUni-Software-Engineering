96c29ca270233b397677ba944d83141e84424b73
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-30 21:02:35 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/09.FunctionalProgramming/05.FilterByAge/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _05.FilterByAge
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Person[] people = new Person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]); 
+               
+                people[i] = new Person();
+                people[i].Name = name;
+                people[i].Age = age;
+            }
+
+            string filter = Console.ReadLine();
+            int filterAge = int.Parse(Console.ReadLine());
+            Func<Person, bool> condition = GetAgeCondition(filter, filterAge);
+
+            Func<Person, string> formatter = GetFormatter(Console.ReadLine()); 
+            PrintPeople(people, condition, formatter);
+        }
+        static Func<Person, string> GetFormatter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return p => $"{p.Name}";
+                case "age":
+                    return p => $"{p.Age}";
+                case "name age":
+                    return p => $"{p.Name} - {p.Age}";
+                default:
+                    return null;
+            }
+        }
+        static Func<Person, bool> GetAgeCondition(string filter, int filterAge)
+        {
+            switch (filter)
+            {
+                case "younger": return p => p.Age < filterAge;
+                case "older": return p => p.Age >= filterAge;
+                default:
+                    return null;
+            }
+        }
+        static void PrintPeople(Person[] people, Func<Person, bool> condition, Func<Person, string> formatter)
+        {
+            foreach (var person in people)
+            {
+                if (condition(person))
+                {
+                    Console.WriteLine(formatter(person));
+                }
+            }
+        }
+    }
+}
