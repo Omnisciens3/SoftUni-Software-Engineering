39f6fdc56897f5dcd6b82b28caa7364480675afe
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-30 21:01:00 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/09.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine()
+                                                                  .Split(", ")
+                                                                  .Select(int.Parse)
+                                                                  .Where(x => x % 2 == 0)
+                                                                  .OrderBy(x => x)
+                                                                  .ToArray()));
+        }
+    }
+}
