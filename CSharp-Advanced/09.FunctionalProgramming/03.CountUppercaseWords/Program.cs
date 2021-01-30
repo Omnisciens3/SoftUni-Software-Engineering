cc8c3cc6cac1d34f21996ebf1d064d11f604e48d
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-30 21:01:46 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/09.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //                              .Where(s => s[0] == s.ToUpper()[0])
+            //                              .ToArray();
+
+            //Console.WriteLine(string.Join("\n", input));
+
+            Func<string, bool> filter = text => Char.IsUpper(text[0]);
+            string text = Console.ReadLine();
+            string[] words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Where(filter).ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
