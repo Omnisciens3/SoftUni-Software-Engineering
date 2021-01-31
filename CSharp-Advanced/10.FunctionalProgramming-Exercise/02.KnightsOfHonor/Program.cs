6a07076c9ed4d7298d7d321b0e175626d5e99adb
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-31 22:07:00 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/10.FunctionalProgramming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<string, string> func = (name) =>
+            //{
+            //    return $"Sir {name}";
+            //};
+
+            //Func<string, string> func = name => $"Sir {name}"; -- Съкратен вариант на горния.
+
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            List<string> newNames = names.Select(name => $"Sir {name}").ToList();
+
+            //Action<T>;
+            Action<List<string>> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, newNames));
+
+            printNames(newNames);
+        }
+        //static string FuncMethod(string name)
+        //{
+        //    return $"Sir {name}";
+        //}
+    }
+}
