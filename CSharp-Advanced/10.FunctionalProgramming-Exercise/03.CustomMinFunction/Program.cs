8a268376ba633b4d270a4f8b5326fb6d5e772342
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-31 22:07:18 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/10.FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int minNumber = numbers.Min();
+            Func<List<int>, int> minNumber =
+                numbers =>
+                {
+                    int minNum = int.MaxValue;
+
+                    foreach (int num in numbers)
+                    {
+                        if (num < minNum)
+                        {
+                            minNum = num;
+                        }
+                    }
+                    return minNum;
+                };
+
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int result = minNumber(numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
