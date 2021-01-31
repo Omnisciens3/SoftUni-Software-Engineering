bb75f78456acb446d8f0caede7aab14795ca4939
Omnisciens <kristian.georgi3v@protonmail.com>
2021-01-31 22:06:40 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/10.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            //Action<T>;
+            Action<List<string>> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printNames(names);
+        }
+    }
+}
