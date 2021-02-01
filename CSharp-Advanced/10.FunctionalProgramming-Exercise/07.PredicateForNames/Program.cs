9bba43b8c1dadb10304b688211189f8542b17500
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-01 22:56:33 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/10.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            //names = names.Where(name => name.Length <= n).ToList();
+            Func<string, bool> func = name => name.Length <= n;
+            names = names.Where(func).ToList();
+
+            Action<List<string>> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+        }
+    }
+}
