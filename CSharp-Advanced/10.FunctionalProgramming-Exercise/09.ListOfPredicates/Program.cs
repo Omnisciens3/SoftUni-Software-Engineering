37905448f8b3f71a380cab5199fba35c4b7f749c
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-01 22:57:09 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Functional Programming)

---
+++ CSharp-Advanced/10.FunctionalProgramming-Exercise/09.ListOfPredicates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers = Enumerable.Range(1, n).ToList();
+
+            Func<int, int, bool> predicate = (num, d) => num % d == 0;
+
+            foreach (int num in numbers)
+            {
+                if (dividers.All(d => predicate(num, d)))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
