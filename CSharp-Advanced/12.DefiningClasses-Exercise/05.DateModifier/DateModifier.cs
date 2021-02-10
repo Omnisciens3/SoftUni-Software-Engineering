c4d584851c203c5343d7bd13c154c29c9d5c23a6
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-10 17:27:41 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Defining Classes - Exercise)

---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifierProblem
+{
+    public static class DateModifier
+    {
+        public static int GetDiffBetweenDates(string dateOne, string dateTwo)
+        {
+            DateTime date1 = DateTime.Parse(dateOne);
+            DateTime date2 = DateTime.Parse(dateTwo);
+
+            TimeSpan difference = date1 - date2;
+
+            return difference.Days;
+        }
+    }
+}
---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DateModifierProblem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.GetDiffBetweenDates(firstDate, secondDate);
+
+            Console.WriteLine(Math.Abs(days));
+        }
+    }
+}
