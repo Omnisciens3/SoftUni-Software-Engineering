f3fea2d27a5a7d2337af8e931817482818714fd8
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-10 17:25:48 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Defining Classes - Exercise)

---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/01.DefineAClassPerson/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        //public Person()
+        //{
+        //    Name = name;
+        //    Age = age;
+        //}
+
+        public string Name 
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age 
+        {
+            get => age;
+            set => age = value;
+        }
+
+    }
+}
---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.Name = "Pesho";
+            personOne.Age = 22;
+
+            Person personTwo = new Person()
+            {
+                Name = "Kris",
+                Age = 22
+            };
+        }
+    }
+}
