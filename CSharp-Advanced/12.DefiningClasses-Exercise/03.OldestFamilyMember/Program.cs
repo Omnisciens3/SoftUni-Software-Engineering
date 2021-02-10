362e1ed36ca72de5db23b51f4e43a1d243ac3f95
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-10 17:27:06 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Defining Classes - Exercise)

---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/03.OldestFamilyMember/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person currentPerson = new Person(name, age);
+
+                family.AddMember(currentPerson);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
