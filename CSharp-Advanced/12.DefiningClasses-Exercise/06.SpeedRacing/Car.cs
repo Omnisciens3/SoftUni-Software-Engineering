20af358e7cd33ebc3d33a85ceb7b6c44f9a032a7
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-11 11:04:28 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Defining Classes)

---
+++ CSharp-Advanced/12.DefiningClasses-Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.SpeedRacing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+        public bool Drive(double distanceTravelled)
+        {
+            double neededFuel = distanceTravelled * FuelConsumptionPerKilometer;
+
+            if (neededFuel > FuelAmount)
+            {
+                return false;
+            }
+
+            TravelledDistance += distanceTravelled;
+            FuelAmount -= neededFuel;
+
+            return true;
+        }
+    }
+}
