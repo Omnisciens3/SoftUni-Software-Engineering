8ee6c06f079f028a88a9bc2d28b6af98b5441f56
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-15 01:05:41 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Generics)

---
+++ CSharp-Advanced/13.Generics/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+
+            Type valueType = Value.GetType();
+
+            string valueTypeFullName = valueType.FullName;
+            return $"{valueTypeFullName}: {Value}";
+        }
+    }
+}
---
+++ CSharp-Advanced/13.Generics/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<double>> doubles = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<double> number = new Box<double>(double.Parse(Console.ReadLine()));
+                doubles.Add(number);
+            }
+
+            double value = double.Parse(Console.ReadLine());
+            Box<double> comparableBox = new Box<double>(value);
+
+            int count = GetGreater(doubles, comparableBox);
+            Console.WriteLine(count);
+
+        }
+        private static int GetGreater<T>(List<Box<T>> boxes, Box<T> box) where T : IComparable
+        {
+            int count = 0;
+
+            foreach (Box<T> item in boxes)
+            {
+                if (item.Value.CompareTo(box.Value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
