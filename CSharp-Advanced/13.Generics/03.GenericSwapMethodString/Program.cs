95b7218b98b3128aaf8ee23e674e4340db28617b
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-14 22:53:16 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Generics)

---
+++ CSharp-Advanced/13.Generics/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>(); 
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SwapIndexes(boxes, indexes[0], indexes[1]);
+
+            foreach (Box<string> box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        private static void SwapIndexes<T>(List<Box<T>> boxes, int firstIndex, int secondIndex)
+        {
+            Box<T> temp = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
