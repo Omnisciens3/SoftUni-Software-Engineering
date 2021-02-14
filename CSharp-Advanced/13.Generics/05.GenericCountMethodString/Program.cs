e582e36debf555a58dc74989c518680285b23c07
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-14 22:54:01 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Generics)

---
+++ CSharp-Advanced/13.Generics/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.GenericCountMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            string value = Console.ReadLine();
+            Box<string> comparableBox = new Box<string>(value);
+
+            int count = GetGreater(boxes, comparableBox);
+            Console.WriteLine(count);
+
+        }
+        private static int GetGreater<T>(List<Box<T>> boxes, Box<T> box) where T : IComparable
+        {
+            int count = 0;
+
+            foreach (Box<T> item in boxes)
+            {
+                if (item.Value.CompareTo(box.Value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
