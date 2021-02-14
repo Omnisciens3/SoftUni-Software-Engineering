088ef2112867b4144b860effb27bd4490ff997fe
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-14 22:53:41 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Generics)

---
+++ CSharp-Advanced/13.Generics/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = Value.GetType();
+
+            string valueTypeFullName = valueType.FullName;
+            return $"{valueTypeFullName}: {Value}";
+        }
+    }
+}
