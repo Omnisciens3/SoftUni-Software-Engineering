887912224d1ffce8ca95d38dcad33a25301dc8a2
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-15 01:06:24 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Generics)

---
+++ CSharp-Advanced/13.Generics/07.Tuple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstTupleData = Console.ReadLine().Split();
+            string fullName = $"{firstTupleData[0]} {firstTupleData[1]}";
+            MyTuple<string, string> tuple =
+                new MyTuple<string, string>(fullName, firstTupleData[2]);
+
+            string[] secondTupleData = Console.ReadLine().Split();
+            MyTuple<string, int> secondTuple =
+                new MyTuple<string, int>(secondTupleData[0], int.Parse(secondTupleData[1]));
+
+            string[] thirdTupleData = Console.ReadLine().Split();
+            MyTuple<int, double> thirdTuple = 
+                new MyTuple<int, double>(int.Parse(thirdTupleData[0]), double.Parse(thirdTupleData[1]));
+
+            Console.WriteLine(tuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
