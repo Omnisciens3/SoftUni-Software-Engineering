c57e06102c9f994aa9c2b21655b1e87f5de9f7d2
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-16 16:09:30 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Iterators & Comparators)

---
+++ CSharp-Advanced/14.IteratorsAndComparators/06.EqualityLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> people = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                sortedPeople.Add(new Person(name, age));
+                people.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(people.Count);
+        }
+    }
+}
