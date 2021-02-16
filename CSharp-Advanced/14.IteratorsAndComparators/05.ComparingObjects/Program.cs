d0d2dae5540b6406be32d484a054d3f90478f11e
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-16 16:09:12 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Iterators & Comparators)

---
+++ CSharp-Advanced/14.IteratorsAndComparators/05.ComparingObjects/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] inputData = input.Split();
+                string name = inputData[0];
+                int age = int.Parse(inputData[1]);
+                string town = inputData[2];
+
+                people.Add(new Person(name, age, town));
+
+                input = Console.ReadLine();
+            }
+
+            int equalPeopleCount = 0;
+            int notEqualPeopleCount = 0;
+
+            int personIndex = int.Parse(Console.ReadLine());
+
+            Person targetPerson = people[personIndex - 1];
+
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(targetPerson) == 0)
+                {
+                    equalPeopleCount++;
+                }
+                else
+                {
+                    notEqualPeopleCount++;
+                }
+            }
+
+            if (equalPeopleCount > 1)
+            {
+                Console.WriteLine($"{equalPeopleCount} {notEqualPeopleCount} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
