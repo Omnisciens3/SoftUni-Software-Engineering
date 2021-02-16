e70f2d271d15934de82c0c877bd10b38da309948
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-16 16:08:17 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Iterators & Comparators)

---
+++ CSharp-Advanced/14.IteratorsAndComparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+            index = 0;
+        }
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = items.ToList();
+            index = 0;
+        }
+
+        public bool Move()
+        {
+            bool hasNext = HasNext();
+
+            if (hasNext)
+            {
+                index++;
+            }
+
+            return hasNext;
+        }
+
+        public void Print()
+        {
+            if (index >= items.Count)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(items[index]);
+        }
+
+        public bool HasNext()
+        {
+            return index < items.Count - 1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
