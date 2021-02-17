d11f7126428ef5ad72ac15eeffa21659a3b49cf9
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-17 12:26:16 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Exam preparation)

---
+++ CSharp-Advanced/15.ExamPreparation/01.FlowerWreaths/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine()
+                                                      .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                      .Select(int.Parse)
+                                                      .ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine()
+                                                      .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                      .Select(int.Parse)
+                                                      .ToArray());
+
+            int wreaths = 0;
+            int extraFlowers = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int currentRose = roses.Dequeue();
+                int currentLillie = lilies.Pop();
+
+                int sum = currentRose + currentLillie;
+
+                if (sum == 15)
+                {
+                    wreaths++;
+                }
+                else if (sum > 15)
+                {
+                    while (true)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreaths++;
+                            break;
+                        }
+                        if (sum < 15)
+                        {
+                            extraFlowers += sum;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    extraFlowers += sum;
+                }
+            }
+
+            wreaths += extraFlowers / 15;
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
