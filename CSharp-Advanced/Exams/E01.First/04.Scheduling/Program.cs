8775b253152d6fee94dff0011ac85ab61bf34cf6
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-18 15:10:35 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Exam prep)

---
+++ CSharp-Advanced/Exams/E01.First/04.Scheduling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                     .Select(int.Parse)
+                                                     .ToArray());
+
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                       .Select(int.Parse)
+                                                       .ToArray());
+
+            int intValue = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                int taskValue = tasks.Peek();
+                int threadValue = threads.Peek();
+
+                if (taskValue == intValue)
+                {
+                    Console.WriteLine($"Thread with value {threadValue} killed task {taskValue}");
+                    break;
+                }
+                else if (threadValue < taskValue)
+                {
+                    threads.Dequeue();
+                }
+                else if (threadValue >= taskValue)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+            }
+
+            foreach (var item in threads)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
