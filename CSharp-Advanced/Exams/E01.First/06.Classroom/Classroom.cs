6f9664f9c6b600d271eaa195c7532a58a163b48f
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-18 15:11:09 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Exam prep)

---
+++ CSharp-Advanced/Exams/E01.First/06.Classroom/Classroom.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity 
+        {
+            get => capacity;
+            set => capacity = value;
+        }
+
+        public int Count { get { return students.Count; } }
+
+        public string RegisterStudent(Student student)
+        {
+            
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+
+            return "No seats in the classroom";
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+            if (students.Contains(student))
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return "Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            bool isThereStudents = false;   
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+
+            foreach (var student in students.Where(s => s.Subject == subject))
+            {
+                sb.AppendLine($"{student.FirstName} {student.LastName}").ToString().TrimEnd();
+                isThereStudents = true;
+            }
+
+            if (isThereStudents)
+            {
+                return sb.ToString().TrimEnd();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+            return student;
+        }
+    }
+}
