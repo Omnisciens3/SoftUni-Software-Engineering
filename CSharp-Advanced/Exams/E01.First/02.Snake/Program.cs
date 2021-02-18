06d53c2acb8ab92988975f0bb09f7c60e6123227
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-18 15:09:36 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Exam prep)

---
+++ CSharp-Advanced/Exams/E01.First/02.Snake/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace _02.Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            int snakeRow = 0;
+            int snakeCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                }
+            }
+
+            int foodEaten = 0;
+
+            while (foodEaten < 10)
+            {
+                string movement = Console.ReadLine();
+
+                matrix[snakeRow, snakeCol] = '.';
+                snakeRow = MoveRow(snakeRow, movement);
+                snakeCol = MoveCol(snakeCol, movement);
+
+                if (!IsPositionValid(snakeRow, snakeCol, n, n))
+                {
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+
+                if (matrix[snakeRow, snakeCol] == '*')
+                {
+                    foodEaten++;
+                }
+
+                if (matrix[snakeRow, snakeCol] == 'B')
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            if (matrix[row, col] == 'B')
+                            {
+                                matrix[snakeRow, snakeCol] = '.'; 
+                                snakeRow = row;
+                                snakeCol = col;
+                            }
+                        }
+                    }
+                }
+
+                matrix[snakeRow, snakeCol] = 'S';
+            }
+
+            if (foodEaten == 10)
+            {
+                Console.WriteLine("You won! You fed the snake.");
+            }
+
+            Console.WriteLine($"Food eaten: {foodEaten}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+    }
+}
