47da6dcf953131e1213247b0197179bb5a1d7280
Omnisciens <kristian.georgi3v@protonmail.com>
2021-02-19 15:00:21 +0200

    Added to SoftUni-Software-Engineering -> CSharp Advanced (Exams)

---
+++ CSharp-Advanced/Exams/E02.Second/06.Heroes/HeroRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            data = new List<Hero>();
+        }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Hero hero)
+        {
+            data.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            Hero hero = data.FirstOrDefault(x => x.Name == name);
+            data.Remove(hero);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            Hero hero = data.OrderByDescending(x => x.Item.Strength).First();
+
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            Hero hero = data.OrderByDescending(x => x.Item.Ability).First();
+
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            Hero hero = data.OrderByDescending(x => x.Item.Intelligence).First();
+
+            return hero;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Hero hero in data)
+            {
+                sb.AppendLine($"Hero: {hero.Name} – {hero.Level}lvl");
+                sb.AppendLine("Item:");
+                sb.AppendLine($"  * Strength: {hero.Item.Strength}");
+                sb.AppendLine($"  * Ability: {hero.Item.Ability}");
+                sb.AppendLine($"  * Intelligence: {hero.Item.Intelligence}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
