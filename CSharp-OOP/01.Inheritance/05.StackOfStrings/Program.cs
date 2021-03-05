43131966bbdbd597bb9e5576fa2243e4eca6763d
Omnisciens <kristian.georgi3v@protonmail.com>
2021-03-05 15:07:59 +0200

    Added to SoftUni-Software-Engineering -> CSharp OOP (Inheritance)

---
+++ CSharp-OOP/01.Inheritance/05.StackOfStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(new List<string>()
+            {
+                "123",
+                "456",
+                "789",
+                "pepi"
+            });
+
+            Console.WriteLine(stack.IsEmpty());
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
