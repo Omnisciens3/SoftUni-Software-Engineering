7cd6b5cf9c3587b4d14bb61fbd5f53ec750391c3
Omnisciens <kristian.georgi3v@protonmail.com>
2021-03-05 15:07:30 +0200

    Added to SoftUni-Software-Engineering -> CSharp OOP (Inheritance)

---
+++ CSharp-OOP/01.Inheritance/03.HierarchicalInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            cat.Meow();
+            cat.Eat();
+            dog.Bark();
+            dog.Eat();
+        }
+    }
+}
