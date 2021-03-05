e7afdf76df669ca52da8f91af1c9d55d69eaa4e1
Omnisciens <kristian.georgi3v@protonmail.com>
2021-03-05 15:07:45 +0200

    Added to SoftUni-Software-Engineering -> CSharp OOP (Inheritance)

---
+++ CSharp-OOP/01.Inheritance/04.RandomList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("Ceci");
+            randomList.Add("Bibi");
+            randomList.Add("Gishi");
+            randomList.Add("Djoni");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomList.RandomString());
+            }
+        }
+    }
+}
