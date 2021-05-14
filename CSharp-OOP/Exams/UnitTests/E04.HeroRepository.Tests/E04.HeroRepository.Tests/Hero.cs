f16df3b6c871b443ca02b1952865691364dde06d
Omnisciens <kristian.georgi3v@protonmail.com>
2021-05-14 13:30:14 +0300

    Added to SoftUni-Software-Engineering -> CSharp OOP (Unit Tests)

---
+++ CSharp-OOP/Exams/UnitTests/E04.HeroRepository.Tests/E04.HeroRepository.Tests/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E04.HeroRepository.Tests
+{
+    public class Hero
+    {
+        public Hero(string name, int level)
+        {
+            this.Name = name;
+            this.Level = level;
+        }
+
+        public string Name { get; }
+
+        public int Level { get; }
+    }
+}
---
+++ CSharp-OOP/Exams/UnitTests/E04.HeroRepository.Tests/E04.HeroRepository.Tests/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E04.HeroRepository.Tests
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
