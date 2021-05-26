3d38b33d895aab5ce06c97bd893717089e1eb7d5
Omnisciens <kristian.georgi3v@protonmail.com>
2021-05-26 11:49:38 +0300

    Added to SoftUni-Software-Engineering -> CSharp OOP (Unit Tests)

---
+++ CSharp-OOP/Exams/UnitTests/E06.PresentTest/E06.PresentTest/Present.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E06.PresentTest
+{
+    public class Present
+    {
+        public Present(string name, double magic)
+        {
+            this.Name = name;
+            this.Magic = magic;
+        }
+
+        public string Name { get; set; }
+
+        public double Magic { get; set; }
+    }
+}
---
+++ CSharp-OOP/Exams/UnitTests/E06.PresentTest/E06.PresentTest/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E06.PresentTest
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
